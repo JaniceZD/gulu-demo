5aa1cf85829ecf8bf33b8df46f386c8dfaa2a019
JaniceZD <mnzd123@gmail.com>
2020-08-14 14:33:55 +0800

    创建官网

--- src/button.vue
+++ src/button.vue
@@ -3,7 +3,7 @@
             @click="$emit('click')">
         <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
         <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
-        <div class="content">
+        <div class="g-button-content">
             <slot></slot>
         </div>
     </button>
@@ -34,6 +34,14 @@
     }
 </script>
 <style lang="scss" scoped>
+    $font-size: 14px;
+    $button-height: 32px;
+    $button-bg: white;
+    $button-active-bg: #eee;
+    $border-radius: 4px;
+    $color: #333;
+    $border-color: #999;
+    $border-color-hover: #666;
     @keyframes spin {
         0% {
             transform: rotate(0deg);
@@ -48,31 +56,28 @@
     }
 
     .g-button {
-        font-size: var(--font-size);
-        height: var(--button-height);
-        padding: 0 1em;
-        border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
-        background: var(--button-bg);
+        font-size: $font-size; height: $button-height; padding: 0 1em;
+        border-radius: $border-radius; border: 1px solid $border-color;
+        background: $button-bg;
+
         display: inline-flex;
         justify-content: center;
         align-items: center;
         vertical-align: top;
 
         &:hover {
-            border-color: var(--border-color-hover);
+            border-color: $border-color-hover;
         }
 
         &:active {
-            border-color: var(--button-active-bg);
+            background-color: $button-active-bg;
         }
 
         &:focus {
             outline: none;
         }
 
-        > .content {
-            vertical-align: top;
+        > .g-button-content {
             order: 2;
         }
 
@@ -88,7 +93,7 @@
                 margin-left: .1em;
             }
 
-            > .content {
+            > .g-button-content {
                 order: 1;
             }
         }
